e7f517a2be7028af95d32d0563d69b83af5181b2
sanglim <sanglim@kookmin.ac.kr>
2022-11-01 19:46:23 +0900

    feat. 세팅 및 비밀번호 변경 페이지 제작 중

--- app/src/main/java/ac/kr/kookmin/petdiary/SettingActivity.java
+++ app/src/main/java/ac/kr/kookmin/petdiary/SettingActivity.java
@@ -1,15 +1,25 @@
 package ac.kr.kookmin.petdiary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class SettingActivity extends AppCompatActivity {
 
+    LinearLayout changePwLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    public void ChangePW(View view) {
+        Intent intent = new Intent(getApplicationContext(), ChangePasswordActivity.class);
+        startActivity(intent);
     }
 }
