83330c568efa55acce0374a48b830e6cf3a76d9f
VarGun <heegun4690@naver.com>
2022-11-07 20:17:24 +0900

    activity_profile_edit, Profile_EditActivity : 성별 선택 spinner 구현 activity_profile_others 추가 heedong1 img 추가

--- app/src/main/java/ac/kr/kookmin/petdiary/ProfileActivity.java
+++ app/src/main/java/ac/kr/kookmin/petdiary/ProfileActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
