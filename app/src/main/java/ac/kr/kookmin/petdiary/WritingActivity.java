20b40d60cf521ce7f8a9a1bec21c31e42c643850
sanglim <sanglim@kookmin.ac.kr>
2022-11-22 20:58:08 +0900

    feat. 앨범에서 이미지 선택 구현 진행중

--- app/src/main/java/ac/kr/kookmin/petdiary/WritingActivity.java
+++ app/src/main/java/ac/kr/kookmin/petdiary/WritingActivity.java
@@ -2,33 +2,47 @@ package ac.kr.kookmin.petdiary;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.InputStream;
 
 
 public class WritingActivity extends AppCompatActivity {
 
-    private static final int PICK_FROM_CAMERA = 0;
-    private static final int PICK_FROM_ALBUM = 1;
-    private static final int CROP_FROM_IMAGE = 2;
 
     BottomNavigationView bottomNavigationView;
-
-
+    ImageView uploadImg;
+    Button uploadImgBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writing);
 
+        uploadImg = findViewById( R.id.Img_upload);
+        uploadImgBtn = findViewById(R.id.btn_uploadImg);
+        uploadImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UploadImg();
+            }
+        });
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -56,8 +70,7 @@ public class WritingActivity extends AppCompatActivity {
         });
 
     }
-
-    public void UploadImg(View view) {
+    private void UploadImg() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("이미지 업로드").setMessage("아래 버튼을 클릭하여 이미지를 업로드 해주세요.");
         builder.setPositiveButton("사진촬영", new DialogInterface.OnClickListener() {
@@ -76,17 +89,27 @@ public class WritingActivity extends AppCompatActivity {
         alertDialog.show();
 
     }
+
+
     public void TakePhoto() {
 
 
     }
     public void TakeAlbum() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 0);
+    }
 
-        Intent intent = new Intent(Intent.ACTION_PICK);
-
-        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-        startActivityForResult(intent, PICK_FROM_ALBUM);
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0) {
+            if (requestCode == RESULT_OK) {
+                Glide.with(getApplicationContext()).load(data.getData()).override(400, 400).into(uploadImg);
+            }
+        }
     }
 
     public void UploadPost(View view) {
