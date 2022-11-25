88f67676ca1e613139cb58acecbb474dcc652a36
jasong <jasong@student.42seoul.kr>
2022-11-25 16:53:36 +0900

    like button 추가 및 download 버튼 동작 구현예정

---
+++ app/cp_MainActivity.java
@@ -0,0 +1,78 @@
+package ac.kr.kookmin.petdiary;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mainView;
+    MainRecyclerAdapter mainAdapter;
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<MainItemList> mainItems = new ArrayList<>();
+        mainView = (RecyclerView) findViewById(R.id.mainRecycler);
+        mainAdapter = new MainRecyclerAdapter();
+        mainView.setAdapter(mainAdapter);
+        mainView.setLayoutManager(new LinearLayoutManager(this));
+
+        for (int i = 0; i < 20; i++) {
+            mainItems.add(new MainItemList("test_username", "테스트용입니다.", "", ""));
+        }
+        mainAdapter.setMainList(mainItems);
+//        mainAdapter.setOnItemClickListener(new MainRecyclerAdapter.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(View v, int position) {
+//            }
+//
+//            @Override
+//            public void onLikeBtnClick(View v, int position) {
+//
+//            }
+//        });
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                switch (item.getItemId()) {
+                    case R.id.action_one:
+                        intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.action_two:
+                        return true;
+                    case R.id.action_three:
+                        intent = new Intent(getApplicationContext(), WritingActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.action_four:
+                        intent = new Intent(getApplicationContext(), NotiActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.action_five:
+                        intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                        startActivity(intent);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
