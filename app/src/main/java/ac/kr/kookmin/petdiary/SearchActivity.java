15ad9d396e063be49a4070b56a7a80ea17379f21
sanglim <sanglim@kookmin.ac.kr>
2022-11-25 15:18:09 +0900

    feat. Search 페이지 제작 시작

---
+++ app/src/main/java/ac/kr/kookmin/petdiary/SearchActivity.java
@@ -0,0 +1,15 @@
+package ac.kr.kookmin.petdiary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
--- app/src/main/java/ac/kr/kookmin/petdiary/SettingActivity.java
+++ app/src/main/java/ac/kr/kookmin/petdiary/SettingActivity.java
@@ -30,20 +30,27 @@ public class SettingActivity extends AppCompatActivity {
                     case R.id.action_one:
                         intent = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(intent);
+                        finish();
                         return true;
                     case R.id.action_two:
+                        intent = new Intent(getApplicationContext(), SearchActivity.class);
+                        startActivity(intent);
+                        finish();
                         return true;
                     case R.id.action_three:
                         intent = new Intent(getApplicationContext(), WritingActivity.class);
                         startActivity(intent);
+                        finish();
                         return true;
                     case R.id.action_four:
                         intent = new Intent(getApplicationContext(), NotiActivity.class);
                         startActivity(intent);
+                        finish();
                         return true;
                     case R.id.action_five:
                         intent = new Intent(getApplicationContext(), ProfileActivity.class);
                         startActivity(intent);
+                        finish();
                         return true;
                 }
                 return false;
