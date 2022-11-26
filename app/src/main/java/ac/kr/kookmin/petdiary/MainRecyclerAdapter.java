4e442e0db283f274019c8a5e62e042d6be852890
j.myeong_ <jmyeong012@gmail.com>
2022-11-26 20:27:24 +0900

    ing. 백엔드 연동 진행중

--- app/src/main/java/ac/kr/kookmin/petdiary/MainRecyclerAdapter.java
+++ app/src/main/java/ac/kr/kookmin/petdiary/MainRecyclerAdapter.java
@@ -12,11 +12,15 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainRecyclerAdapter extends RecyclerView.Adapter<MainRecyclerAdapter.ViewHolder> {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     private ArrayList<MainItemList> mainList;
     @NonNull
@@ -77,9 +81,14 @@ public class MainRecyclerAdapter extends RecyclerView.Adapter<MainRecyclerAdapte
 
                         pos = getAdapterPosition();
                         uid = mainList.get(pos).getUid();
-                        Toast.makeText(itemView.getContext(), uid, Toast.LENGTH_SHORT).show();
-                        intent = new Intent(view.getContext(), ProfileActivity.class);
-                        view.getContext().startActivity(intent);
+                        String userUid = mAuth.getCurrentUser().getUid();
+                        if (uid != null && userUid != null && uid.equals(userUid)) {
+                            intent = new Intent(view.getContext(), ProfileActivity.class);
+                            view.getContext().startActivity(intent);
+                        } else {
+                            intent = new Intent(view.getContext(), Profile_OthersActivity.class);
+                            view.getContext().startActivity(intent);
+                        }
                     }
                 }
             });
