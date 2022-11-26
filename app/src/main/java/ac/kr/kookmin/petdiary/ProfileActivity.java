90a02c88fcbd13e3d3800da124c4687cd3c7509c
sanglim <sanglim@kookmin.ac.kr>
2022-11-26 19:00:09 +0900

    feat. footer 로직 수정

--- app/src/main/java/ac/kr/kookmin/petdiary/ProfileActivity.java
+++ app/src/main/java/ac/kr/kookmin/petdiary/ProfileActivity.java
@@ -99,7 +99,7 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Intent intent;
-                finish();
+                if(item.getItemId() != R.id.action_five) finish();
                 switch (item.getItemId()) {
                     case R.id.action_one:
                         intent = new Intent(getApplicationContext(), MainActivity.class);
